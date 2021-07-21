47f75baba5d88bf76648b617c29ea31b7ad7b490
Paul Omondi <paulo.techie@gmail.com>
2021-07-21 10:37:53 +0300

    Modify form eventlistener

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,5 @@
+import loadWeather from "./weather-api";
+
 const displayData = (data) => {
   const main = document.getElementById('content');
   const header = document.createElement('header');
@@ -30,29 +32,36 @@ const displayData = (data) => {
   sectionTemp.appendChild(pT);
   sectionMain.appendChild(img);
   sectionMain.appendChild(h2);
-
+    
   form.appendChild(label);
   form.appendChild(input);
   form.appendChild(button);
 
-  form.setAttribute('id', 'input-form');
-  input.setAttribute('placeholder', 'Enter City ...');
+  input.setAttribute('placeholder', 'Enter City');
   input.setAttribute('type', 'text');
   input.setAttribute('id', 'cityname');
   input.setAttribute('name', 'cityname');
   sectionMain.setAttribute('id', 'weatherdesc');
-
+  
   label.innerText = 'Weather';
   button.innerText = 'Search';
+
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    let city = input.value;
+    sectionDiv.style.display = 'none';
+    header.style.display = 'none';
+    loadWeather(city);
+  });
 
   const arr = Array.from(data.weather);
   h2.innerText = arr[0].description;
   img.setAttribute('src', `http://openweathermap.org/img/wn/${arr[0].icon}@2x.png`);
   img.setAttribute('alt', arr[0].description);
   cityh4.innerText = data.name;
-  countryh4.innerHTML = '<i class = "fas fa-map-marker-alt"></i>';
-  h3.innerHTML = `${Math.round(data.main.temp - 273.15)} &#8451;`;// convert Kelvin to degrees celsius
-  pT.innerHTML = '<i class = "fas fa-thermometer-full"></i>';
-};
+  countryh4.innerHTML= '<i class="fas fa-map-marker-alt"></i>';
+  h3.innerHTML = `${Math.round(data.main.temp - 273.15)} &#8451;`; // convert Kelvin to degrees celsius
+  pT.innerHTML = '<i class="fas fa-thermometer-full"></i>';
+}
 
 export default displayData;
